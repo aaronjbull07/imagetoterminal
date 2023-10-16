6e9b2394b51172a04edc286247c6be6ea3f16b7d
nogarddev <115876934+nogarddev@users.noreply.github.com>
2023-10-16 15:33:08 +0100

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,12 @@
+namespace main
+{
+    internal class Program
+    {
+        static void Main(string args[]) {
+            
+        }
+        static int[] divider(int size, int amount) {
+            
+        }
+    }
+}
